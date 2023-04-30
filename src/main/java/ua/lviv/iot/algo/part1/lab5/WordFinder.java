ab17f3c7a5203e338dafa698e36a3a7fa5a87541
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-04-30 20:09:43 +0300

    Update WordFinder.java

--- src/main/java/ua/lviv/iot/algo/part1/lab5/WordFinder.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/WordFinder.java
@@ -1,10 +1,12 @@
 package ua.lviv.iot.algo.part1.lab5;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordFinder {
+
     public static List<String> findWords(final String text, final int digit) {
         List<String> result = new ArrayList<>();
         Pattern pattern = Pattern.compile("\\b\\w" + digit + "\\w*\\b");
