a06abc3fe71e63fddf2ef75763ab2aa671e6521b
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-05-01 12:20:44 +0300

    Update WordFinderTest.java

--- src/test/java/ua/lviv/iot/algo/part1/lab5/WordFinderTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab5/WordFinderTest.java
@@ -1,27 +1,17 @@
 package ua.lviv.iot.algo.part1.lab5;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WordFinderTest {
-
     @Test
-    public void testFindWordsWithMultipleResults() {
-        String text = "This is a test sentence.";
-        int digit = 5;
-        List<String> words = WordFinder.findWords(text, digit);
-        Assertions.assertEquals(0, words.size());
-        Assertions.assertFalse(words.contains("test"));
-        Assertions.assertFalse(words.contains("sentence"));
-    }
-
-    @Test
-    public void testFindWordsWithNoResults() {
-        String text = "This is a test sentence.";
-        int digit = 9;
-        List<String> words = WordFinder.findWords(text, digit);
-        Assertions.assertTrue(words.isEmpty());
+    void testFindWords() {
+        WordFinder finder = new WordFinder();
+        String text = "This is a test input 123";
+        List<String> expected = Arrays.asList();
+        List<String> actual = finder.findWords(text, 3);
+        assertEquals(expected, actual);
     }
 }
